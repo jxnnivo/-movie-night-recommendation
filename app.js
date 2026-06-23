b02175099d57e27e8a77d95cdd6c7c1a21e09222
Jennifer <jxnnivo@gmail.com>
2026-06-23 16:57:32 -0400

    Fix port for deployment, add real movie recommendations, mark link as optional

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { readPosts, writePosts } from './data.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let recommendations = readPosts();
 
